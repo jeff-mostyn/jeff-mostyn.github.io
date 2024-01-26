b4aa047baee96dbe29aedd4565217eb96d57f998
Jeff Mostyn <jeffmostyn2@gmail.com>
2024-01-26 01:05:43 -0500

    tailwind works

--- portfolio/src/components/Nav.tsx
+++ portfolio/src/components/Nav.tsx
@@ -2,6 +2,8 @@ import React from "react";
 
 export const Nav = () => {
     return (
-        <></>
+        <div className="flex h-16 bg-white border border-b border-green-900">
+
+        </div>
     )
 }
