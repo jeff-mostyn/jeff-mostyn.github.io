4cf6432103915e4cf1280b598d62a65b175f682c
Jeff Mostyn <jeffmostyn2@gmail.com>
2024-02-02 13:09:41 -0500

    basic image modals on work samples

--- portfolio/src/components/Portfolio/FeatureDisplay.tsx
+++ portfolio/src/components/Portfolio/FeatureDisplay.tsx
@@ -36,7 +36,7 @@ export type PortfolioItemData = {
 	path: string;
 }
 
-const labelStyles = (alignLeft: boolean) => classNames("absolute flex flex-row justify-center w-full bottom-0 items-end pb-6 px-12 md:pb-12 md:px-16", {
+const labelStyles = (alignLeft: boolean) => classNames("absolute flex flex-row justify-center w-full bottom-0 items-end pb-6 px-12 md:pb-12 md:px-16 ", {
 	"md:justify-start": alignLeft,
 	"md:justify-end": !alignLeft
 });
@@ -50,6 +50,7 @@ type FeatureDisplayProps = {
 export const FeatureDisplay = ( { data, alignLeft, setActiveProject } : FeatureDisplayProps ) => {
 	return (
 		<div className="relative w-full h-64 md:h-72">
+			<div className="absolute w-full h-full bg-black opacity-10"/>
 			<img className="w-full h-full object-cover object-center" src={data.banner.image} alt="bannerImage"/>
 			<div className={labelStyles(alignLeft)}>
 				<div className={`flex flex-col items:center ${alignLeft ? 'md:items-start' : 'md:items-end'}`}>
--- portfolio/src/components/Portfolio/WorkSample.tsx
+++ portfolio/src/components/Portfolio/WorkSample.tsx
@@ -11,35 +11,46 @@ export type WorkSampleContent = {
 
 export const WorkSample = ({ image, title, description }: WorkSampleContent) => {
   const [detailsOpen, setDetailsOpen] = useState<boolean>(false);
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
 
   return (
-    <div className="w-full">
-      <div className="px-6">
-        <img className="aspect-video rounded" src={image} alt={`${title} Sample`}/>
-        <div className="pt-4 text-white text-xl font-urbanist">
-          {title}
+    <>
+      <div className={`${modalOpen ? 'fixed' : 'hidden'} flex flex-col top-0 right-0 left-0 justify-center items-center w-full h-full`}>
+        <button className="absolute text-white top-0 right-0 pt-6 pr-6" onClick={() => setModalOpen(false)}>
+          X
+        </button>
+        <img className="flex w-3/4 aspect-video rounded-md top-0 right-0 left-0" src={image} />
+        <div className="fixed w-full h-full top-0 bg-black opacity-60 -z-10" onClick={() => setModalOpen(false)}/>
+      </div>
+
+      <div className="w-full">
+        <div className="px-6">
+          <img className="aspect-video rounded cursor-pointer" src={image} alt={`${title} Sample`} onClick={() => setModalOpen(true)}/>
+          <div className="pt-4 text-white text-xl font-urbanist">
+            {title}
+          </div>
+        </div>
+        {detailsOpen && <div className="mt-3.5">
+          {description.map((descrip, key) => {
+            return (
+              <div className="px-4 text-white text-base font-urbanist text-left indent-6" key={key}>
+                {descrip}
+              </div>
+            )  
+          })}
+        </div>}
+        <div className="w=full px-4 mt-3.5">
+          <button
+            type="button"
+            className="w-full h-5 px-6 transition-colors duration-150 bg-zinc-700 focus:shadow-outline hover:bg-zinc-750"
+            onClick={() => setDetailsOpen(!detailsOpen)}
+          >
+            <div className="flex w-full justify-center">
+              <SVG name={detailsOpen ? "triangle_up" : "triangle_down"} size="md" />
+            </div>
+          </button>
         </div>
       </div>
-      {detailsOpen && <div className="mt-3.5">
-        {description.map((descrip, key) => {
-          return (
-            <div className="px-3 text-white text-base font-urbanist text-left indent-8" key={key}>
-              {descrip}
-            </div>
-          )  
-        })}
-      </div>}
-      <div className="w=full px-3 mt-3.5">
-        <button
-          type="button"
-          className="w-full h-5 px-6 transition-colors duration-150 bg-zinc-700 focus:shadow-outline hover:bg-zinc-750"
-          onClick={() => setDetailsOpen(!detailsOpen)}
-        >
-          <div className="flex w-full justify-center">
-            <SVG name={detailsOpen ? "triangle_up" : "triangle_down"} size="md" />
-          </div>
-        </button>
-      </div>
-    </div>
+    </>
   )
 }
