0ee96feeb1f19e7793a83790b818b368d05d9c95
Jeff Mostyn <jeffmostyn2@gmail.com>
2024-02-24 22:41:14 -0500

    adding feature gif

--- portfolio/src/_data/Portfolio/RacingGame/racingGame.js
+++ portfolio/src/_data/Portfolio/RacingGame/racingGame.js
@@ -2,6 +2,7 @@ import banner from './racingGame_banner.png'
 import main_menu_flow from './main_menu_flow.gif'
 import keyImage from './racingGame_keyImage.png'
 import characterSelect from './racingGame_characterSelect.png'
+import racing_game_feature from './racing_game_feature.gif';
 
 import unreal_notext from '../../../media/unreal_notext.png'
 import cpp from '../../../media/cpp.png'
@@ -25,7 +26,7 @@ export const racingGameData = {
     ],
     "summary": "Our currently unannounced anti-grav racer seeks to emulate the adrenaline of F-Zero's lightning-fast gameplay, while also incorporating the accesibility and casual fun of Mario Kart. Gain advantage with the careful use of items and your vehicle's boost gauge, but be careful as your opponents have access to the same dangerous tools. Weave through the skyscrapers of the cities of future America at breakneck speed as you aim to become the greatest racer alive.",
     "hyperlinks": [],
-    "keyImage": keyImage,
+    "keyImage": racing_game_feature,
     "involvement": [
       "For this project, I explored aspects of development that were new to me, and deepened my experience with others. My Master's degree program at the University of Pennsylvania was my first exposure to shader writing, and the visual effects here, like the toon-styled vehicles and abundant holograms, were the perfect chance to put that learning into practice. I also did more in the realm of UI/UX than I ever had in a game before, moving beyond just writing its code to also create the designs, effects, and much of the art myself, excluding the characters.",
       "Furthermore, I've programmed a number of gameplay mechanics and systems, done animations, and used my cross-disciplinary experience to bridge the gap between the tech and art specialists.",
