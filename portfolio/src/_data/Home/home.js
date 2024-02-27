07dfcb1338664567b3294cacc7c1a96b8d9be002
Jeff Mostyn <jeffmostyn2@gmail.com>
2024-02-26 22:30:17 -0500

    firefox doesn't like bold tags

--- portfolio/src/_data/Home/home.js
+++ portfolio/src/_data/Home/home.js
@@ -4,10 +4,18 @@ import profilePhoto from './Headshot1.png'
 export const data = {
   photo: profilePhoto,
   header: "Hello! My name is Jeff Mostyn, career software engineer and hobbyist game developer.",
+  // bio: [
+  //   "<b>Front-end Engineer of 4.5 years</b> at Comcast in Philadelphia", 
+  //   "Master's student at the <b>University of Pennsylvania</b>, studying Computer Graphics & Game Technology",
+  //   "Drexel University graduate, with a <b>Bachelor's of Science with Honors in Computer Science</b> & concentration in Game Programming", 
+  //   "My favorite games include Magic: the Gathering, SMITE (soon SMITE 2?), and TES IV: Oblivion",
+  //   "Music lover - I've played saxophone since the 4th grade, as well as guitar and bass",
+  //   "I picked up learning Japanese as a hobby during COVID and have passed up to JLPT N4, though there's much room for improvement."
+  // ]
   bio: [
-    "<b>Front-end Engineer of 4.5 years</b> at Comcast in Philadelphia", 
-    "Master's student at the <b>University of Pennsylvania</b>, studying Computer Graphics & Game Technology",
-    "Drexel University graduate, with a <b>Bachelor's of Science with Honors in Computer Science</b> & concentration in Game Programming", 
+    "Front-end Engineer of 4.5 years at Comcast in Philadelphia", 
+    "Master's student at the University of Pennsylvania, studying Computer Graphics & Game Technology",
+    "Drexel University graduate, with a Bachelor's of Science with Honors in Computer Science & concentration in Game Programming", 
     "My favorite games include Magic: the Gathering, SMITE (soon SMITE 2?), and TES IV: Oblivion",
     "Music lover - I've played saxophone since the 4th grade, as well as guitar and bass",
     "I picked up learning Japanese as a hobby during COVID and have passed up to JLPT N4, though there's much room for improvement."
