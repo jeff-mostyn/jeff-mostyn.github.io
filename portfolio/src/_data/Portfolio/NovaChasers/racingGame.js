241f16bd5a635fe7a8b720af5a0ff79d5968a40b
Jeff Mostyn <jeffmostyn2@gmail.com>
2024-10-19 21:46:20 -0400

    build?

--- portfolio/src/_data/Portfolio/NovaChasers/racingGame.js
+++ portfolio/src/_data/Portfolio/NovaChasers/racingGame.js
@@ -52,15 +52,15 @@ export const racingGameData = {
     ],
     "skills": [],
     "sections": [
-      {
-        "title": "Main Menu Flow",
-        "image": main_menu_flow,
-        "description": [
-          "When I began to design the main menu, I asked the project director what sort of style he envisioned for it, and the suggestion that was given to me was to aim for clean and futuristic, with just a little rebellious flavor. With that, and without so much as a wireframe, I began to lay things out in Unreal's Slate UI to start envisioning where I could go with the design.",
-          "Ultimately, I chose to keep controls to the left of the screen so as to allow the right half for displaying information and thematic flair. As for design, I began with sharp-angled black and white elements, and lit up selected buttons with a yellow-orange gradient which hearkens back to our iconography, while having a sharp black shadow strike behind them on selection. This splashy treatment makes for sharp contrast next to the simplicity of the other UI elements, and incorporates a litle of that 'rebellious' feeling."
-        ],
-        "imageLeft": false
-      },
+      // {
+      //   "title": "Main Menu Flow",
+      //   "image": main_menu_flow,
+      //   "description": [
+      //     "When I began to design the main menu, I asked the project director what sort of style he envisioned for it, and the suggestion that was given to me was to aim for clean and futuristic, with just a little rebellious flavor. With that, and without so much as a wireframe, I began to lay things out in Unreal's Slate UI to start envisioning where I could go with the design.",
+      //     "Ultimately, I chose to keep controls to the left of the screen so as to allow the right half for displaying information and thematic flair. As for design, I began with sharp-angled black and white elements, and lit up selected buttons with a yellow-orange gradient which hearkens back to our iconography, while having a sharp black shadow strike behind them on selection. This splashy treatment makes for sharp contrast next to the simplicity of the other UI elements, and incorporates a litle of that 'rebellious' feeling."
+      //   ],
+      //   "imageLeft": false
+      // },
       {
         "title": "Character Select",
         "image": characterSelect,
@@ -68,7 +68,7 @@ export const racingGameData = {
           "My goal when designing the character select screen was to maintain readability of the roster while prominently highlighting our character and vehicle art, and to do so with a sleek, stylish UI. To do this, I often referred to spacing and arrangement decisions made in other character-focused games, like Overwatch and MiHoYo's Genshin Impact and Honkai: Star Rail.",
           "The character cards in particular went through a number of design iterations. Initially with much sharper corners and font styles, I opted to round things out a little bit while also adding additional subtle detail to the cards, specifically the gradient background fields which have had subtle \"digitization\" and noise effects applied. The screen is still a work in progress, and I hope to add more to the right half of the screen, including unique wordmarks for the characters' names."
         ],
-        "imageLeft": true
+        "imageLeft": false
       },
       {
         "title": "Racing Heads-Up-Display",
@@ -77,7 +77,7 @@ export const racingGameData = {
           "The process of designing the Heads-Up Display mandated that I balance a number of asks with the need to maintain readability. Specifically, two requirements were that character portraits be highly visible, and that it be abundantly clear when a character holds an item. Pushing either of those elements too far would block visbility in a fast-paced, visually busy game, so finding a way to compromise was key.",
           "In doing so, I found a way to address another issue that we were discovering. We had found that opponents were often hard to see on the track, so rather than fix full-size portraits to the side of the viewport, which I found to be a readability concern, I made it so nearby opponents had their portrait and placement hovering above them. This helped to avoid cluttering the HUD, while also keeping our characters in the forefront."
         ],
-        "imageLeft": false
+        "imageLeft": true
       },
       
     ]
