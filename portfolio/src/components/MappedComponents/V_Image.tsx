599603b947cff3d1d7a30c758b741d3caad794d7
Jeff Mostyn <jeffmostyn2@gmail.com>
2025-12-20 16:20:01 -0500

    try to have images keep aspect ratio

--- portfolio/src/components/MappedComponents/V_Image.tsx
+++ portfolio/src/components/MappedComponents/V_Image.tsx
@@ -2,6 +2,6 @@ import { Vis_Img } from "src/utils/portfolioSegmentTypes";
 
 export const V_Image: React.FC<Vis_Img> = ({ image }: Vis_Img) => {
   return (
-    <img className="md:px-4 lg:px-0 mx-auto aspect-video rounded-md" src={image} alt={"Key"} />
+    <img className="md:px-4 lg:px-0 mx-auto w-max h-auto rounded-md" src={image} alt={"Key"} />
   )
 }
