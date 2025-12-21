6be58f6ada5949ca8446a702a8c365cb09d320ad
Jeff Mostyn <jeffmostyn2@gmail.com>
2025-12-20 23:44:58 -0500

    fix spacing

--- portfolio/src/components/Nav/index.tsx
+++ portfolio/src/components/Nav/index.tsx
@@ -33,12 +33,12 @@ export const Nav = () => {
 
 	return (
 		<div className="
-			fixed items-center z-30 left-0 right-0 bottom-8 mx-auto h-fit max-w-96 w-fit
+			fixed z-30 left-0 right-0 bottom-8 mx-auto h-fit max-w-96 w-fit
 			sm:left-0
 			md:gap-6 md:mr-8 md:top-4
 		">
 			<div className={`flex flex-col ${hasNotif && 'bg-theme1'} rounded-t-2xl rounded-xl h-fit shadow-[0px_4px_8px_1px_rgba(0,_0,_0,_0.65)]`}>
-				<div className="flex flex-row px-8 py-2 w-fit h-fit gap-4 bg-material-neutral-secondary rounded-xl">
+				<div className="flex flex-row items-center px-6 py-2 w-fit h-fit gap-4 bg-material-neutral-secondary rounded-xl">
 					<NavLinkWrapper route={"/"} label={
 						GetLocalizedContentValue(!!activePortfolioItem ? labels.close : labels.portfolio)}
 					/>
@@ -49,7 +49,7 @@ export const Nav = () => {
 					>
 						{GetLocalizedContentValue(labels.resume)}
 					</a>
-					<a className="flex self-center pt-0.5" href="https://www.linkedin.com/in/jeffreymostyn/">
+					<a className="flex self-center" href="https://www.linkedin.com/in/jeffreymostyn/">
 						<SVG name="linkedin" size={Sizes.MD} baseColor="white" transition={true} transitionColor="theme1"/>
 					</a>
 					<div className="flex w-6 h-6 items-center justify-center transition-colors duration-300 bg-white hover:bg-theme1 rounded-md">
